5745857dc8b6eb9834a738c57d79715a7e8b9f24
S-4-S <zv.const@gmail.com>
2020-04-07 13:43:32 +0100

    Change assets folder location

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
-      assets: path.resolve(__dirname, 'assets/'),
+      assets: path.resolve(__dirname, 'src/assets/'),
       components: path.resolve(__dirname, 'src/components/')
     }
   }
