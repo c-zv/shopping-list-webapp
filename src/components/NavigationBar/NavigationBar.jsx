6cf33e7209ba7cba12d2b8724aefaaa4009291f7
S-4-S <zv.const@gmail.com>
2020-04-10 12:17:48 +0100

    Replace simple navbar with bootstrap navbar
    - Add logo

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Route, Switch, NavLink } from 'react-router-dom';
+import {
+  Route, Switch, NavLink, Link,
+} from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 import Home from 'components/Home';
 import Products from 'components/Products';
 import ShoppingLists from 'components/ShoppingLists';
 import Settings from 'components/Settings';
+import logo from 'assets/logo.png';
 import styles from './navigationBar.scss';
 
 const NavigationBar = () => (
   <>
-    <div className={styles.navBar}>
-      <NavLink className={styles.navLink} to="/">Home</NavLink>
-      <NavLink className={styles.navLink} to="/products">Products</NavLink>
-      <NavLink className={styles.navLink} to="/shopping-lists">My shopping lists</NavLink>
-      <NavLink className={styles.navLink} to="/settings">Settings</NavLink>
-    </div>
+    <Navbar bg="dark" variant="dark" expand="sm">
+      <Navbar.Brand as={Link} to="/">
+        <img src={logo} alt="Logo" width="50" height="30" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/products">Products</Nav.Link>
+          <Nav.Link as={NavLink} to="/shopping-lists">My shopping lists</Nav.Link>
+          <Nav.Link as={NavLink} to="/settings">Settings</Nav.Link>
+        </Nav>
+
+      </Navbar.Collapse>
+    </Navbar>
 
     <Switch>
       <Route exact path="/">
