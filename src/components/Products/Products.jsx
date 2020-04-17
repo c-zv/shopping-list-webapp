13e184b16b87bfb76feb512924db759bce453ed5
S-4-S <zv.const@gmail.com>
2020-04-17 15:45:26 +0100

    Improve products page
    - Create responsive card list
    - Move state logic to productHook

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import {
+  Container, Card, Row, Col,
+} from 'react-bootstrap';
 
-import { actProducts } from 'state/products';
+import styles from './products.scss';
 import useProductsHook from './hooks';
 
 const Products = ({ goTo }) => {
-  const { products, randomProduct } = useProductsHook();
-  const dispatch = useDispatch();
-
+  const { products, addProduct } = useProductsHook();
   return (
     <>
       <div>
-        Products tab!
+        <button type="button" onClick={addProduct}> Create random product </button>
       </div>
-      <div>
-        <button type="button" onClick={() => (dispatch(actProducts.addProduct(randomProduct())))}> Create random product </button>
-      </div>
-      <ul>
-        {products.map((prod) => (
-          <li key={prod.id}>
-            <Link to={goTo.PRODUCT(prod.id)}>{prod.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <Container fluid="xl">
+        <Row xs={1} sm={1} md={1} lg={2} xl={3} noGutters>
+          {products.map((prod) => (
+            <Col as={Card} className={styles.card} key={prod.id}>
+              <Card.Img className={styles.image} variant="top" src={prod.imageLink} />
+              <Card.Body as={Link} to={goTo.PRODUCT(prod.id)}>
+                <Card.Title>{prod.name}</Card.Title>
+              </Card.Body>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
     </>
   );
 };
--- src/components/Products/hooks/ProductsHook.jsx
+++ src/components/Products/hooks/ProductsHook.jsx
@@ -1,14 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { actProducts } from 'state/products';
+
+const randomProduct = () => ({
+  id: `${Math.round(Math.random() * 100000)}`,
+  name: `name_${Math.round(Math.random() * 1000).toString()}`,
+  imageLink: 'http://placeimg.com/300/160/tech',
+});
 
 const useProductsHook = () => {
   const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
 
-  const randomProduct = () => ({
-    id: `${Math.round(Math.random() * 100000)}`,
-    name: `name_${Math.round(Math.random() * 1000).toString()}`,
-  });
+  const addProduct = useCallback(
+    () => (dispatch(actProducts.addProduct(randomProduct()))),
+    [dispatch],
+  );
 
-  return { products, randomProduct };
+  return { products, addProduct };
 };
 
 export default useProductsHook;
