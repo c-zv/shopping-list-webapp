6800f9630e486a777590ec289f386c51cf6097b4
S-4-S <zv.const@gmail.com>
2020-04-18 22:14:36 +0100

    Replace bootstrap by ant-design in Products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
-  Container, Card, Row, Col,
-} from 'react-bootstrap';
+  Button, Card, Row, Col,
+} from 'antd';
 
 import styles from './products.scss';
 import useProductsHook from './hooks';
@@ -13,22 +13,27 @@ const Products = ({ goTo }) => {
   return (
     <>
       <div>
-        <button type="button" onClick={addProduct}> Create random product </button>
+        <Button type="primary" onClick={addProduct}>
+          Create random product
+        </Button>
       </div>
-      <Container fluid="xl">
-        <Row xs={1} sm={2} md={3} lg={3} xl={5} noGutters>
-          {products.map((prod) => (
-            <Col key={prod.id}>
-              <Card text="white" bg="dark" className={styles.card}>
-                <Card.Img className={styles.image} variant="top" src={prod.imageLink} />
-                <Card.Body as={Link} to={goTo.PRODUCT(prod.id)}>
-                  <Card.Text>{prod.name}</Card.Text>
-                </Card.Body>
+
+      <Row justify="start" gutter={[16, 16]}>
+        {products.map((prod) => (
+          <Col key={prod.id} justify="center">
+            <Link to={goTo.PRODUCT(prod.id)}>
+              <Card
+                className={styles.card}
+                hoverable
+                bordered
+                cover={<img alt="" src={prod.imageLink} className={styles.image} />}
+              >
+                {prod.name}
               </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
