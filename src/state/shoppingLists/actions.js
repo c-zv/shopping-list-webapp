60f4a790eb5b67d9092c6aad3bc0509c04b5760c
S-4-S <zv.const@gmail.com>
2020-04-22 21:22:27 +0100

    Define new actions for shopping lists
    - new actions and reducers to edit and delete a shopping list

--- src/state/shoppingLists/actions.js
+++ src/state/shoppingLists/actions.js
@@ -1,12 +1,24 @@
 
 export const types = {
   ADD_SHOPPING_LIST: 'ADD_SHOPPING_LIST',
+  EDIT_SHOPPING_LIST: 'EDIT_SHOPPING_LIST',
+  DELETE_SHOPPING_LIST: 'DELETE_SHOPPING_LIST',
 };
 
 const addShoppingList = (shoppingList) => (
   { type: types.ADD_SHOPPING_LIST, payload: shoppingList }
 );
 
+const editShoppingList = (shoppingList) => (
+  { type: types.EDIT_SHOPPING_LIST, payload: shoppingList }
+);
+
+const deleteShoppingList = (id) => (
+  { type: types.DELETE_SHOPPING_LIST, payload: id }
+);
+
 export const actionsShopLists = {
   addShoppingList,
+  editShoppingList,
+  deleteShoppingList,
 };
--- src/state/shoppingLists/reducers.js
+++ src/state/shoppingLists/reducers.js
@@ -4,6 +4,10 @@ const reducerShopLists = (shoppingLists = [], action) => {
   switch (action.type) {
     case types.ADD_SHOPPING_LIST:
       return [...shoppingLists, action.payload];
+    case types.EDIT_SHOPPING_LIST:
+      return shoppingLists.map((sl) => (sl.id === action.payload.id ? { ...action.payload } : sl));
+    case types.DELETE_SHOPPING_LIST:
+      return shoppingLists.filter((sl) => sl.id !== action.payload);
     default:
       return shoppingLists;
   }
