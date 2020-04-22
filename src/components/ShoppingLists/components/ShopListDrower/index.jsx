a46d483b91a8daf2e2303a5675930a535fd68bbe
S-4-S <zv.const@gmail.com>
2020-04-22 21:34:53 +0100

    Update ShopListDriver component to support edit and create mode
    - for edit mode the form is filled with existing shop list information
    - for create mode the form is blank

--- src/components/ShoppingLists/components/ShopListDrower/index.jsx
+++ src/components/ShoppingLists/components/ShopListDrower/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   Drawer, Form, Input, Button, Select,
@@ -6,21 +6,57 @@ import {
 
 import styles from './shopListDrower.scss';
 
-const ShopListDrower = ({ drowerCtrl }) => {
+const categories = [
+  {
+    name: 'Tech',
+    color: '#8da0cb',
+  },
+  {
+    name: 'Food',
+    color: '#fc8d62',
+  },
+  {
+    name: 'Regular',
+    color: '#66c2a5',
+  },
+];
+
+const ShopListDrower = ({ drowerCtrl, submitShopList }) => {
   const [form] = Form.useForm();
 
   const submitForm = useCallback((values) => {
-    drowerCtrl.createShopList(values);
+    const updatedShopList = { ...drowerCtrl.shopListToEdit, ...values };
+    submitShopList(updatedShopList);
     form.resetFields();
     drowerCtrl.close();
   },
-  [drowerCtrl, form]);
+  [submitShopList, drowerCtrl, form]);
+
+  useEffect(
+    () => (
+      drowerCtrl.shopListToEdit
+        ? form.setFieldsValue(drowerCtrl.shopListToEdit)
+        : form.resetFields()
+    ),
+    [form, drowerCtrl.shopListToEdit],
+  );
+
+  let titleText;
+  let submitText;
+  if (drowerCtrl.shopListToEdit) {
+    titleText = 'Edit shopping list';
+    submitText = 'Update';
+  } else {
+    titleText = 'New shopping list';
+    submitText = 'Create';
+  }
 
   return (
     <Drawer
-      title="New shopping list"
+      title={titleText}
       placement="right"
       closable
+      getContainer={false}
       onClose={drowerCtrl.close}
       visible={drowerCtrl.visible}
     >
@@ -35,20 +71,21 @@ const ShopListDrower = ({ drowerCtrl }) => {
         </Form.Item>
 
         <Form.Item
-          name="category"
+          name="color"
           label="Category"
           hasFeedback
           rules={[{ required: true, message: 'Please select a category' }]}
         >
           <Select placeholder="Category">
-            <Select.Option value="china">China</Select.Option>
-            <Select.Option value="usa">U.S.A</Select.Option>
+            {categories.map((cat) => (
+              <Select.Option value={cat.color}>{cat.name}</Select.Option>
+            ))}
           </Select>
         </Form.Item>
 
         <div className={styles.buttons}>
           <Button type="primary" htmlType="submit">
-            Create
+            {submitText}
           </Button>
           <Button type="default" onClick={() => form.resetFields()}>
             Reset
@@ -64,8 +101,9 @@ ShopListDrower.propTypes = {
   drowerCtrl: PropTypes.shape({
     close: PropTypes.func.isRequired,
     visible: PropTypes.bool.isRequired,
-    createShopList: PropTypes.func.isRequired,
+    shopListToEdit: PropTypes.shape({}),
   }).isRequired,
+  submitShopList: PropTypes.func.isRequired,
 };
 
 export default ShopListDrower;
