863c83fa990b1f865e72ffdc24dfb229d355a820
S-4-S <zv.const@gmail.com>
2020-05-07 20:23:07 +0100

    Change store configurations to incorporate sagas middleware

--- src/state/index.js
+++ src/state/index.js
@@ -1,21 +1,36 @@
-import { createStore, combineReducers } from 'redux';
+import {
+  createStore, applyMiddleware, combineReducers, compose,
+} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
+
 import { reducerProducts } from './products';
-import { reducerShopLists } from './shoppingLists';
+import { reducerShopLists, sagaShopLists } from './shoppingLists';
 
 export const rootReducer = combineReducers({
   products: reducerProducts,
   shoppingLists: reducerShopLists,
 });
 
-/* eslint-disable no-underscore-dangle */
-export const initStore = () => {
-  const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+function* rootSaga() {
+  yield all([
+    sagaShopLists(),
+  ]);
+}
+
+export const initStore = (initialState = {}) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const middlewares = [sagaMiddleware];
+  const enhancer = composeEnhancers(
+    applyMiddleware(...middlewares),
   );
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
+
+  const store = createStore(rootReducer, initialState, enhancer);
+  if (process.env.NODE_ENV === 'development' && module.hot) {
     module.hot.accept('./', () => store.replaceReducer(rootReducer));
   }
+  sagaMiddleware.run(rootSaga);
   return store;
 };
-/* eslint-enable */
