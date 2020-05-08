3c44099bf62d46d380e9df2ed802bc21fd35b3d9
c.zv <zv.const@gmail.com>
2020-05-08 22:02:10 +0100

    Use notification manager to show actions results

--- src/state/shoppingListCategories/sagas.js
+++ src/state/shoppingListCategories/sagas.js
@@ -1,23 +1,18 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
 
 import api from 'utils/api';
+import Notification from 'utils/notificationManager';
 import { types, actionsShopListCategories } from './actions';
 
-// TODO: replace this method with global notifications
-const showGlobalNotification = (errorMessage) => {
-  const defaultMessage = 'Something went wrong. Please try again later';
-  const message = errorMessage || defaultMessage;
-  console.log('--> showGlobalNotification.2: ', message);
-};
-
 function* requestCategoriesAll() {
+  const notification = new Notification();
   try {
     const result = yield call(api.categories.getAll);
     yield put(actionsShopListCategories.categoriesAll.requestSuccess(result.data));
   } catch (error) {
     yield put(actionsShopListCategories.categoriesAll.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
--- src/state/shoppingLists/sagas.js
+++ src/state/shoppingLists/sagas.js
@@ -1,71 +1,75 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
 
 import api from 'utils/api';
+import Notification from 'utils/notificationManager';
 import { types, actionsShopLists } from './actions';
 
-
-// TODO: replace this method with global notifications
-const showGlobalNotification = (errorMessage) => {
-  const defaultMessage = 'Something went wrong. Please try again later';
-  const message = errorMessage || defaultMessage;
-  console.log('--> showGlobalNotification: ', message);
-};
-
 function* requestShopListsAll() {
+  const notification = new Notification();
   try {
     const result = yield call(api.shopLists.getAll);
     yield put(actionsShopLists.shopListAll.requestSuccess(result.data));
   } catch (error) {
     yield put(actionsShopLists.shopListAll.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
 function* requestShopListsOne(action) {
+  const notification = new Notification();
   try {
     const result = yield call(api.shopLists.getOne, action.payload);
     yield put(actionsShopLists.shopListOne.requestSuccess(result.data));
   } catch (error) {
     yield put(actionsShopLists.shopListOne.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
 function* requestShopListsCreate(action) {
+  const notification = new Notification();
   try {
+    notification.loading('Creating shopping list...');
     yield call(api.shopLists.create, action.payload);
     yield put(actionsShopLists.shopListCreate.requestSuccess());
+    notification.success('Shopping list created!');
     yield put(actionsShopLists.shopListAll.request());
   } catch (error) {
     yield put(actionsShopLists.shopListCreate.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
 function* requestShopListsUpdate(action) {
+  const notification = new Notification();
   try {
+    notification.loading('Updating shopping list...');
     yield call(api.shopLists.update, action.payload.id, action.payload.shopList);
     yield put(actionsShopLists.shopListUpdate.requestSuccess());
+    notification.success('Shopping list updated!');
     yield put(actionsShopLists.shopListAll.request());
   } catch (error) {
     yield put(actionsShopLists.shopListUpdate.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
 function* requestShopListsDelete(action) {
+  const notification = new Notification();
   try {
+    notification.loading('Deleting shopping list...');
     yield call(api.shopLists.delete, action.payload);
     yield put(actionsShopLists.shopListDelete.requestSuccess());
+    notification.success('Shopping list deleted!');
     yield put(actionsShopLists.shopListAll.request());
   } catch (error) {
     yield put(actionsShopLists.shopListDelete.requestFail());
     const errorMessage = error.response ? error.response.data.error : error.message;
-    showGlobalNotification(errorMessage);
+    notification.error(errorMessage);
   }
 }
 
