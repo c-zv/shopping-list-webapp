6eb3205f115da28996cf086a8219f7abbae45c1e
c.zv <zv.const@gmail.com>
2020-07-17 16:46:54 +0100

    Create generic api request hook

---
+++ src/api/useApiRequestHook.jsx
@@ -0,0 +1,65 @@
+import {
+  useState, useEffect, useCallback, useReducer,
+} from 'react';
+
+const requestActions = {
+  FETCH: 'REQUEST_INIT',
+  SUCCESS: 'REQUEST_SUCCESS',
+  ERROR: 'REQUEST_ERROR',
+};
+
+const requestReducer = (state, action) => {
+  switch (action.type) {
+    case requestActions.FETCH:
+      return {
+        ...state,
+        error: undefined,
+        loading: true,
+      };
+    case requestActions.SUCCESS:
+      return {
+        response: { data: action.payload },
+        error: undefined,
+        loading: false,
+      };
+    case requestActions.ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const useApiRequestHook = (requestFn, argsFn = []) => {
+  const [request] = useState(() => requestFn);
+  const [requestArgs] = useState(argsFn);
+
+  const [requestState, dispatch] = useReducer(requestReducer, {
+    response: {},
+    error: undefined,
+    loading: false,
+  });
+
+  const apiRequest = useCallback(
+    async () => {
+      dispatch({ type: requestActions.FETCH });
+      try {
+        const res = await request(...requestArgs);
+        dispatch({ type: requestActions.SUCCESS, payload: res.data });
+      } catch (err) {
+        dispatch({ type: requestActions.ERROR, payload: err });
+      }
+    }, [request, requestArgs],
+  );
+
+  useEffect(() => {
+    apiRequest();
+  }, [apiRequest]);
+
+  return requestState;
+};
+
+export default useApiRequestHook;
