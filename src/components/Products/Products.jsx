3a940d47b74a2cedfc591b8c1ad05461a30ded82
c.zv <zv.const@gmail.com>
2020-07-21 11:41:06 +0100

    Update Products page with structure and logic

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,32 +2,125 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
-  Card, Row, Col, Spin,
+  Card, Spin, Button, Select, InputNumber,
 } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
 
 import styles from './products.scss';
 import useProductsHook from './hooks';
 
 const Products = ({ goTo }) => {
-  const { products, loading } = useProductsHook();
+  const {
+    products,
+    loading,
+    showProductModal,
+    modalLoading,
+    openModal,
+    handleAddToShopList,
+    myShopLists,
+    selectedShopList,
+    setSelectedShopList,
+    setQuantity,
+  } = useProductsHook();
+
   return (
     <Spin size="large" spinning={loading}>
-      <Row justify="start" gutter={[16, 16]}>
-        {products.map((prod) => (
-          <Col key={prod.id} justify="center">
-            <Link to={goTo.PRODUCT(prod.id)}>
-              <Card
-                className={styles.card}
-                hoverable
-                bordered
-                cover={<img alt="" src="http://placeimg.com/190/200/tech" className={styles.image} />}
-              >
-                {prod.product.name}
-              </Card>
-            </Link>
-          </Col>
+      <div className={styles.productsWrapper}>
+        {products.map((prod, index) => (
+          <Card
+            key={prod.id}
+            className={styles.card}
+            hoverable
+            bordered={false}
+            bodyStyle={{ padding: '0rem', height: '100%' }}
+          >
+            <div className={styles.cardContent}>
+              <div className={styles.imageContainer}>
+                <img alt="" src="http://placeimg.com/220/260/tech" className={styles.imageContainer__image} />
+                <div className={styles.imageContainer__overlay}>
+                  {!showProductModal[index] && (
+                    <Button
+                      type="link"
+                      shape="round"
+                      className={styles.imageContainer__overlay__button}
+                      disabled={modalLoading}
+                      onClick={() => openModal(index)}
+                    >
+                      Add to shopping list
+                    </Button>
+                  )}
+                  {showProductModal[index] && (
+                    <div className={styles.selectList}>
+                      <Select
+                        className={styles.selectList__select}
+                        onChange={(value) => setSelectedShopList(value)}
+                        placeholder="Shopping list"
+                        value={selectedShopList}
+                        allowClear
+                        disabled={modalLoading}
+                      >
+                        {myShopLists.map((shopList) => (
+                          <Select.Option value={shopList.id} key={shopList.id}>
+                            {shopList.name}
+                          </Select.Option>
+                        ))}
+                        <Select.Option value={33} key={4874}>
+                          Random super large name
+                        </Select.Option>
+                      </Select>
+                      <InputNumber
+                        bordered={false}
+                        min={1}
+                        max={50}
+                        defaultValue={1}
+                        prefix={<CloseOutlined />}
+                        formatter={(value) => `x ${value}`}
+                        parser={(value) => value.replace('x ', '')}
+                        className={styles.selectList__quantity}
+                        onChange={(value) => setQuantity(value)}
+                        disabled={modalLoading}
+                      />
+                      <div className={styles.selectList__buttons}>
+                        <Button
+                          type="primary"
+                          onClick={() => handleAddToShopList(true, index)}
+                          loading={modalLoading}
+                          disabled={!selectedShopList}
+                        >
+                          Add
+                        </Button>
+                        <Button
+                          type="danger"
+                          onClick={() => handleAddToShopList(false, index)}
+                          disabled={modalLoading}
+                        >
+                          Cancel
+                        </Button>
+
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+              </div>
+              <Link to={goTo.PRODUCT(prod.id)} className={styles.cardInfo}>
+                <span className={styles.cardInfo__name}>
+                  {prod.product.name}
+                </span>
+                <span className={styles.cardInfo__price}>
+                  { `${prod.price} €` }
+                </span>
+                <div className={styles.cardInfo__seller}>
+                  <span className={styles.cardInfo__sellerName}>
+                    { `Store ${prod.store_id}` }
+                  </span>
+                  <div className={styles.stockIndicator} />
+                </div>
+              </Link>
+            </div>
+          </Card>
         ))}
-      </Row>
+      </div>
     </Spin>
   );
 };
