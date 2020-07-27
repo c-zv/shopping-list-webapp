5999503e181ad4c17edac36dc66a370f62d13ace
c.zv <zv.const@gmail.com>
2020-07-27 10:34:57 +0100

    Implement a hook to manage the logic for adding a new item to shop list

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,5 +1,7 @@
 import useApiRequestHook from './useApiRequestHook';
+import useAddShopListItemHook from './useAddShopListItemHook';
 
 export {
   useApiRequestHook,
+  useAddShopListItemHook,
 };
---
+++ src/hooks/useAddShopListItemHook.jsx
@@ -0,0 +1,43 @@
+import {
+  useState, useEffect, useMemo, useCallback,
+} from 'react';
+
+import api from '~/api';
+import useApiRequestHook from './useApiRequestHook';
+
+const useAddShopListItemHook = () => {
+  const {
+    response: shopListsResp, execute: getShopLists,
+  } = useApiRequestHook(api.shopLists.getAll);
+  const myShopLists = useMemo(() => shopListsResp.data || [], [shopListsResp]);
+  useEffect(
+    () => { getShopLists([]); },
+    [getShopLists],
+  );
+
+  const {
+    loading: addingItem, execute: addItem,
+  } = useApiRequestHook(api.shopLists.addItem);
+
+  const [quantity, setQuantity] = useState(1);
+  const [selectedShopList, setSelectedShopList] = useState(undefined);
+
+  const addShopListItem = useCallback(
+    async (storeProductId) => {
+      await addItem([selectedShopList, storeProductId, quantity]);
+      setQuantity(1);
+    }, [addItem, quantity, selectedShopList],
+  );
+
+  return {
+    myShopLists,
+    addingItem,
+    quantity,
+    setQuantity,
+    selectedShopList,
+    setSelectedShopList,
+    addShopListItem,
+  };
+};
+
+export default useAddShopListItemHook;
